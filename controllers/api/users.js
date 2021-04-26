fc202b13544f53874f6e0c0d10db4231e455b377
andrewhuang192 <andrewhuang192@gmail.com>
2021-04-26 14:45:47 -0700

    MERN-Stack infrastructure

--- controllers/api/users.js
+++ controllers/api/users.js
@@ -5,6 +5,7 @@ const bcrypt = require('bcrypt');
 module.exports = {
 	create,
 	login,
+	checkToken,
 };
 
 /*--- Helper Functions ---*/
@@ -41,3 +42,9 @@ async function login(req, res) {
 		res.status(400).json('Bad Credentials');
 	}
 }
+
+function checkToken(req, res) {
+	// req.user will always be there for you when a token is sent
+	console.log('req.user', req.user);
+	res.json(req.exp);
+  }
